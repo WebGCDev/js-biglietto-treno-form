ace64eb7464a365b6bf1358f4dff50afba8ffc1b
WebGCDev <gaetano.chi@gmail.com>
2023-11-07 20:14:11 +0100

    feat: add road to MILESTONE 1

---
+++ js/script.js
@@ -0,0 +1,9 @@
+//DICHIARAZIONI
+const priceForKm = 0.21;
+const underDiscount = 20;
+const overDiscount = 40;
+const name = document.getElementById('name');
+const km = document.getElementById('km');
+const age = document.getElementById('age');
+const btnGener = document.getElementById('btn-gener');
+const btnCancel = document.getElementById('cancel');
